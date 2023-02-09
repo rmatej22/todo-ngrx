8f1408d332c47a1accc93c27408f32acb7de95c7
rmatej22 <rmatej545@gmail.com>
2023-02-09 23:06:48 +0100

    fill list with data from store

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { TodoElement } from '../models/todo-element';
+import { selectTodoElements } from '../store/todo.selector';
+import * as TodoActions from '../store/todo.actions';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   todo: string = '';
-  allTodos$: Observable<TodoElement[]> | undefined;
+  allTodos$: Observable<TodoElement[]> = this.store.select(selectTodoElements);
 
-  constructor() {}
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(TodoActions.loadTodos());
+  }
+
+  addTodo() {
+    this.store.dispatch(TodoActions.addTodo({ content: this.todo }));
+    this.todo = '';
+  }
+
+  deleteTodo(id: string) {
+    this.store.dispatch(TodoActions.removeTodo({ id }));
+  }
 }
