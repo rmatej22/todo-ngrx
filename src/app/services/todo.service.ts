26ab7c6bc18c37415e902b384ac7b334b0414c66
rmatej22 <rmatej545@gmail.com>
2023-02-10 19:49:19 +0100

    create logic for todo.service for effects usage

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { TodoElement } from '../models/todo-element';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  private storageInitialised = false;
 
-  constructor() { }
+  constructor(private storage: Storage) {}
+
+  async getTodos(): Promise<TodoElement[]> {
+    if (!this.storageInitialised) await this.storage.create();
+
+    return (await this.storage.get('todos')) || [];
+  }
+
+  async saveTodos(todos: TodoElement[]) {
+    if (!this.storageInitialised) await this.storage.create();
+
+    return this.storage.set('todos', todos);
+  }
 }
