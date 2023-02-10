38505255163c2738f4893a76a458804f50839de4
rmatej22 <rmatej545@gmail.com>
2023-02-10 20:07:05 +0100

    add load todos effect

--- src/app/store/todo.effects.ts
+++ src/app/store/todo.effects.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, from, map, of, switchMap } from 'rxjs';
+import { TodoService } from '../services/todo.service';
+import * as TodoActions from '../store/todo.actions';
 
 @Injectable()
-export class TodoEffects {}
+export class TodoEffects {
+  constructor(private actions$: Actions, private todoService: TodoService) {}
+
+  // effect for load todos action
+  loadTodos$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(TodoActions.loadTodos),
+      switchMap(() => {
+        return from(this.todoService.getTodos()).pipe(
+          map((todos) => TodoActions.loadTodosSuccess({ todos: todos })),
+          catchError((error) => of(TodoActions.loadTodosFail({ error: error })))
+        );
+      })
+    );
+  });
+}
